5dbd94012347fb09e7b8e0e07b4163699573ce04
LT_Name <572413378@qq.com>
2022-11-17 21:43:08 +0800

    将表情id保存到文件

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 
 已知的表情id：  
-Wave:4c8ae710-df2e-47cd-814d-cc7bf21a3d67  
-The Woodpunch:42fde774-37d4-4422-b374-89ff13a6535a  
-Simple Clap:9a469a61-c83b-4ba9-b507-bdbe64430582  
-Over There:ce5c0300-7f03-455d-aaf1-352e4927b54d  
+
+|     name      |                  id                  |
+|:-------------:|:------------------------------------:|
+|    Waving     | 4c8ae710-df2e-47cd-814d-cc7bf21a3d67 |
+| The Woodpunch | 42fde774-37d4-4422-b374-89ff13a6535a |
+|   Clapping    | 9a469a61-c83b-4ba9-b507-bdbe64430582 |
+|  Over There   | ce5c0300-7f03-455d-aaf1-352e4927b54d |
+|    Follow     | 17428c4c-3813-4ea1-b3a9-d6a32f83afca |
+|   Faceplant   | 6d9f24c0-6246-4c92-8169-4648d1981cbb |
+|  The Pickaxe  | d7519b5a-45ec-4d27-997c-89d402c6b57f |
+|  The Hammer   | 7cec98d8-55cc-44fe-b0ae-2672b0b2bd37 |
+
--- src/main/java/cn/lanink/getemoteid/Main.java
+++ src/main/java/cn/lanink/getemoteid/Main.java
@@ -6,21 +6,47 @@ import cn.nukkit.event.server.DataPacketReceiveEvent;
 import cn.nukkit.network.protocol.EmoteListPacket;
 import cn.nukkit.network.protocol.EmotePacket;
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.Config;
+
+import java.util.ArrayList;
 
 /**
  * @author lt_name
  */
 public class Main extends PluginBase implements Listener {
 
+    private Config log;
+    private final ArrayList<String> emoteList = new ArrayList<>();
+
     @Override
     public void onEnable() {
+        this.log = new Config(this.getDataFolder() + "/log.yml", Config.YAML);
+
+        this.emoteList.clear();
+        this.emoteList.addAll(this.log.getStringList("emoteList"));
+
         this.getServer().getPluginManager().registerEvents(this, this);
+
+        this.getServer().getScheduler().scheduleRepeatingTask(this, this::saveEmoteList, 20 * 60 * 5); // 5分钟保存一次
+    }
+
+    @Override
+    public void onDisable() {
+        this.saveEmoteList();
+    }
+
+    public void saveEmoteList() {
+        this.log.set("emoteList", this.emoteList);
+        this.log.save();
     }
 
     @EventHandler
     public void onDataPacketReceive(DataPacketReceiveEvent event) {
-        if (event.getPacket() instanceof EmoteListPacket ||
-                event.getPacket() instanceof EmotePacket) {
+        if (event.getPacket() instanceof EmoteListPacket) {
+            this.getLogger().info(event.getPacket().toString());
+        }else if (event.getPacket() instanceof EmotePacket) {
+            EmotePacket emotePacket = (EmotePacket) event.getPacket();
+            this.emoteList.add(emotePacket.emoteID);
             this.getLogger().info(event.getPacket().toString());
         }
     }
